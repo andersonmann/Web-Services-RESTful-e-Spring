ab1c59f0eb72f009574be1938a2b6a1d7360180d
Anderson Mann <anderson.civil@hotmail.com>
2016-05-26 23:44:46 -0300

    Módulo :Criando uma API RESTful 2.11. Finalizando o tratamento das respostas

---
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/repository/LivrosRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.algaworks.socialbooks.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.algaworks.socialbooks.domain.Livro;
+
+/**
+ * @author amann
+ *
+ */
+public interface LivrosRepository extends JpaRepository<Livro, Long>{
+
+}
